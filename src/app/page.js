5de3a89f3be9f05937dedd7004e4ed519f90bb22
NGUYEN Minh <minh.nguyen@ddth.corp>
2023-08-08 13:27:58 +0200

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,34 @@
+'use client'
+import styles from './page.module.scss'
+import { useState } from 'react';  
+import { motion } from 'framer-motion';
+import useMousePosition from './utils/useMousePosition';
+
+export default function Home() {
+
+  const [isHovered, setIsHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = isHovered ? 400 : 40;
+
+  return (
+    <main className={styles.main}>
+      <motion.div 
+        className={styles.mask}
+        animate={{
+          WebkitMaskPosition: `${x - (size/2)}px ${y - (size/2)}px`,
+          WebkitMaskSize: `${size}px`,
+        }}
+        transition={{ type: "tween", ease: "backOut", duration:0.5}}
+      >
+          <p onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}}>
+            I LOVE SUCKING THEIR DICKS.
+          </p>
+      </motion.div>
+
+      <div className={styles.body}>
+        <p>I <span>LOVE TALKING</span> with my homies :D</p>
+      </div>
+
+    </main>
+  )
+}
